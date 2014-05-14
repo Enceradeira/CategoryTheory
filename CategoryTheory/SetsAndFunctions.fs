70350e3d2198d9cf18b19b1cebf8a09ab26181fe
Jorg Jenni <joerg.jenni@garaio.com>
2014-05-14 15:36:44 +0100

    inital commit

---
+++ CategoryTheory/SetsAndFunctions.fs
@@ -0,0 +1,16 @@
+﻿module SetsAndFunctions
+
+open NUnit.Framework
+
+
+[<TestFixture>]
+type SetsAndFunctionsFixture() = class
+    
+        [<Test>]
+        [<TestCaseSource("TestCasesForAdd")>]
+        member this.SetsOfAllEvenNumbers() =
+            Assert.That(0, Is.EqualTo 1)
+
+
+end
+
