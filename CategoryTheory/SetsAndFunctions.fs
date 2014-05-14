321702d205e3a47c8706fad059bf5700bd94c264
Jorg Jenni <joerg.jenni@garaio.com>
2014-05-14 16:24:34 +0100

    SetsOfAllEvenNumbers as sequence implemented

--- CategoryTheory/SetsAndFunctions.fs
+++ CategoryTheory/SetsAndFunctions.fs
@@ -3,14 +3,24 @@
 open NUnit.Framework
 
 
+
 [<TestFixture>]
 type SetsAndFunctionsFixture() = class
     
         [<Test>]
-        [<TestCaseSource("TestCasesForAdd")>]
         member this.SetsOfAllEvenNumbers() =
-            Assert.That(0, Is.EqualTo 1)
-
-
+            let rec evenNumbers() = seq { 
+                                        yield 0
+                                        for n in evenNumbers() do yield n+2
+                                      }
+            
+            let evenNumbersFrom0To10 = Seq.take 10 (evenNumbers())
+            Assert.That(evenNumbersFrom0To10, Has.No.Member 1)
+            Assert.That(evenNumbersFrom0To10, Has.Member 2)
+            Assert.That(evenNumbersFrom0To10, Has.No.Member 3)
+            Assert.That(evenNumbersFrom0To10, Has.Member 4)
+            Assert.That(evenNumbersFrom0To10, Has.No.Member 5)
+            Assert.That(evenNumbersFrom0To10, Has.Member 6)
+            Assert.That(evenNumbersFrom0To10, Has.No.Member 7)
 end
 
