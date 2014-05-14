faf9bae0bc078a50a7f33d4deade108afda5109f
Jorg Jenni <joerg.jenni@garaio.com>
2014-05-14 17:53:18 +0100

    SetsOfAllOddNumbers as sequence implemented

--- CategoryTheory/SetsAndFunctions.fs
+++ CategoryTheory/SetsAndFunctions.fs
@@ -2,19 +2,25 @@
 
 open NUnit.Framework
 
+ // Sets and Functions
+
+let SetOfNumbers() = Seq.initInfinite(fun i->i)
+ 
+let rec SetOfEvenNumbers() = seq {  
+                                yield 0
+                                for n in SetOfEvenNumbers() do yield n+2
+                                  }
+                             
+let SetOfOddNumbers() = Seq.map(fun i->i+1) (SetOfEvenNumbers())
 
 
+// Tests
 [<TestFixture>]
 type SetsAndFunctionsFixture() = class
     
         [<Test>]
-        member this.SetsOfAllEvenNumbers() =
-            let rec evenNumbers() = seq { 
-                                        yield 0
-                                        for n in evenNumbers() do yield n+2
-                                      }
-            
-            let evenNumbersFrom0To10 = Seq.take 10 (evenNumbers())
+        member this.SetOfAllEvenNumbers() =
+            let evenNumbersFrom0To10 = Seq.take 10 (SetOfEvenNumbers())
             Assert.That(evenNumbersFrom0To10, Has.No.Member 1)
             Assert.That(evenNumbersFrom0To10, Has.Member 2)
             Assert.That(evenNumbersFrom0To10, Has.No.Member 3)
@@ -22,5 +28,16 @@ type SetsAndFunctionsFixture() = class
             Assert.That(evenNumbersFrom0To10, Has.No.Member 5)
             Assert.That(evenNumbersFrom0To10, Has.Member 6)
             Assert.That(evenNumbersFrom0To10, Has.No.Member 7)
+
+        [<Test>]
+        member this.SetOfOddNumbers() = 
+            let oddNumbersFrom0To10 = Seq.take 10 (SetOfOddNumbers())
+            Assert.That(oddNumbersFrom0To10, Has.Member 1)
+            Assert.That(oddNumbersFrom0To10, Has.No.Member 2)
+            Assert.That(oddNumbersFrom0To10, Has.Member 3)
+            Assert.That(oddNumbersFrom0To10, Has.No.Member 4)
+            Assert.That(oddNumbersFrom0To10, Has.Member 5)
+            Assert.That(oddNumbersFrom0To10, Has.No.Member 6)
+            Assert.That(oddNumbersFrom0To10, Has.Member 7)
 end
 
